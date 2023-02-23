d69ff15108d86474c02fab10252db0b10d96611c
Gustavo Fernandes <gfernandesilva2904@gmail.com>
2023-02-23 19:07:04 -0300

    alteração do arquivo index.blade.php

--- portifolio/resources/views/index.blade.php
+++ portifolio/resources/views/index.blade.php
@@ -2,7 +2,15 @@
 @section('title','Gustavo')
 @section('content')
     
-
+<x-system.navbar/>
+<x-system.header/>
+<x-system.about/>
+<x-system.service/>
+<x-system.portfolio/>
+<x-system.pricing/>
+<x-system.contact/>
+<x-system.testimonial/>
+<x-system.footer/>
     
 
     
