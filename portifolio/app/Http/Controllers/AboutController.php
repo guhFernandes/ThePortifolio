6f374be523c6b96b11cb4e04db0f52406337507c
Gustavo Fernandes <gfernandesilva2904@gmail.com>
2023-02-24 19:59:56 -0300

    atualização do controller

--- portifolio/app/Http/Controllers/AboutController.php
+++ portifolio/app/Http/Controllers/AboutController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\About;
 
 class AboutController extends Controller
 {
-    //
+    public function create (Request $request) {
+
+    }
+
+    public function getAboutAll () {
+        return About::all();
+    }
+
+    public function getAbout (Request $request) {
+        return About::find($request->id);
+    }
+
+    public function updateAbout (Request $request) {
+
+    }
+
+    public function deleteAbout (Request $request) {
+
+    }
+
+    public function search(Request $request) {
+
+    }
 }
